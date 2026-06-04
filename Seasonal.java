172387031f74569616ed11e4b9792ab6ada707ca
Rimorzell <rimorzell@gmail.com>
2026-06-04 17:07:45 +0200

    changes happen

--- Seasonal.java
+++
@@ -1,9 +0,0 @@
-// Seasonal.java
-/*
-  Teil vom module lifecycle. Dinge mit Jahres-Zyklus.
-  STYLE: OO interface, wird vom yearly control flow benutzt.
-*/
-public interface Seasonal {
-    void startSeason(); // Saisonstart (reset von Saisonzuständen)
-    void applyWinter(java.util.Random rng); // Wintereffekte mit RNG
-}
